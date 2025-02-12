a7b1152953a38505232e652c3e0e53968d5096a6
Cris1037 <128873608+Cris1037@users.noreply.github.com>
2025-02-12 11:37:24 -0700

    Cristian Feb 12

--- App.tsx
+++ App.tsx
@@ -1,49 +1,11 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, TextInput, View } from 'react-native';
-// import Signin from './components/signin';
-
-
-
-// export default function App() {
-//   return (
-//     <View>
-//       <Signin booleanToggle={(isLoggedIn: boolean) => {}} username="defaultUsername" setUsername={() => {}} />
-//       <Text>Welcome! Login to access your account.</Text>
-//       <TextInput placeholder="Username" value="defaultUsername" onChangeText={() => {}} />
-//       <StatusBar style="auto" />
-//       <TextInput placeholder="Password" value="" onChangeText={() => {}} />
-
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Signin from './components/signin';
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signin">
-        <Stack.Screen name="Signin" component={SigninScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+
 
 function SigninScreen({ navigation }: { navigation: any }) {
   return (
@@ -58,14 +20,7 @@ function SigninScreen({ navigation }: { navigation: any }) {
   );
 }
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to the Home Screen!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
 
 const styles = StyleSheet.create({
   container: {
